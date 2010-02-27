ea43b985330a3ed292b6acd2b5384b92b8e452bd
Per Christian B. Viken <perchr@northblue.org>
2010-02-27 17:56:32 +0100

    move LOAD_PATH-manipulation into init

--- init.rb
+++ init.rb
@@ -1,6 +1,7 @@
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), "lib"))
+
 begin
   # Try to require the preresolved locked set of gems.
-  puts "Requiring #{File.expand_path('.bundle/environment')}"
   require File.expand_path('.bundle/environment')
 rescue LoadError
   # Fall back on doing an unlocked resolve at runtime.
