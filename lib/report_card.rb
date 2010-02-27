0028417f0826a4cab64ea949e85ca6c810cbd00e
Per Christian B. Viken <perchr@northblue.org>
2010-02-27 18:50:43 +0100

    not using ReportCard::Index for anything

--- lib/report_card.rb
+++ lib/report_card.rb
@@ -3,7 +3,6 @@ $:.unshift(File.dirname(__FILE__))
 require 'report_card/core_ext/kernel'
 require 'report_card/helpers'
 require 'report_card/app'
-require 'report_card/index'
 require 'report_card/grader'
 
 module ReportCard
@@ -14,17 +13,13 @@ module ReportCard
     require config['integrity_path'] + '/init.rb'
 
     ignore = config['ignore'] ? Regexp.new(config['ignore']) : /[^\w\d\s\S]+/
-    projects = []
 
     Integrity::Project.all.each do |project|
       if project.name !~ ignore
         grader = Grader.new(project, config)
         grader.grade
-        projects << project if grader.success?
       end
     end
-
-    Index.create(projects, config['site']) unless projects.empty?
   end
 
   def self.config
