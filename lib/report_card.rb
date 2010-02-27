26c8691d1cb6438168b101e44136646c7d601161
Per Christian B. Viken <perchr@northblue.org>
2010-02-27 19:39:07 +0100

    no automatic grading, so just require the Integrity-files. also, no need for the ignore-thingy

--- lib/report_card.rb
+++ lib/report_card.rb
@@ -6,17 +6,8 @@ require 'report_card/grader'
 module ReportCard
   CONFIG_FILE = File.expand_path(File.join(File.dirname(__FILE__), "..", "config.yml"))
 
-  def self.grade
+  def self.setup
     require config['integrity_path'] + '/init.rb'
-
-    ignore = config['ignore'] ? Regexp.new(config['ignore']) : /[^\w\d\s\S]+/
-
-    Integrity::Project.all.each do |project|
-      if project.name !~ ignore
-        grader = Grader.new(project, config)
-        grader.grade
-      end
-    end
   end
 
   def self.config
