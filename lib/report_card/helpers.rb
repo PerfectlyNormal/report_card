477af64f4acbacfacc46f00e35f99e53992a297c
Per Christian B. Viken <perchr@northblue.org>
2010-02-27 22:50:54 +0100

    authorization, again Integrity comes to the rescue with readymade code

--- lib/report_card/helpers.rb
+++ lib/report_card/helpers.rb
@@ -1,8 +1,10 @@
+require 'report_card/helpers/authorization'
 require 'report_card/helpers/rendering'
 require 'report_card/helpers/urls'
 
 module ReportCard
   module Helpers
+    include Authorization
     include Rendering
     include Urls
 
