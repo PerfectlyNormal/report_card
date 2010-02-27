d7241eb39b619d3ee07c5e20cdd3460f71ba381d
Per Christian B. Viken <perchr@northblue.org>
2010-02-27 22:11:42 +0100

    create a logger

--- lib/report_card.rb
+++ lib/report_card.rb
@@ -6,8 +6,19 @@ require 'report_card/grader'
 module ReportCard
   CONFIG_FILE = File.expand_path(File.join(File.dirname(__FILE__), "..", "config.yml"))
 
+  class << self
+    attr_accessor :logger
+  end
+
   def self.setup
     require config['integrity_path'] + '/init.rb'
+    ReportCard.logger = Logger.new(config['log'] || STDERR)
+    ReportCard.logger.formatter = Logger::Formatter.new # So we get some timestamps as well. Always nice to have
+  end
+
+  def self.log(message, level = :info)
+    level = :info unless [:debug, :info, :warn, :error, :fatal, :unknown].include?(level)
+    logger.send(level, message)
   end
 
   def self.config
